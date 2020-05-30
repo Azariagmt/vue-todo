2e4c388a1f19e58d38bb00d4a38175d495b8a926
Azaria Gebremichael <azariagebremichael10@gmail.com>
2020-05-30 14:58:37 +0300

    calendar size supposed to be fixed

---
+++ .history/src/views/Home_20200530145739.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="home">
+    <v-container fluid>
+      <v-layout row wrap>
+        <v-flex md3 class="d-none d-sm-none d-md-flex calendar">
+          <v-container>
+            <SingleDatePicker />
+          </v-container>
+        </v-flex>
+        <v-flex md6>
+          <v-layout column xs12 md8 lg8 class="mt-5">
+            <v-card text>
+              <todo-list></todo-list>
+            </v-card>
+          </v-layout>
+        </v-flex>
+        <v-flex md3 class="d-none d-sm-none d-md-flex">
+          <v-container>
+            <right-side />
+          </v-container>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TodoList from "@/components/todo components/TodoList.vue";
+import SingleDatePicker from "vue-single-date-picker";
+import RightSide from "@/components/RightSide.vue";
+
+export default {
+  name: "Home",
+  data: function() {
+    return {
+  
+    };
+  },
+  components: {
+    TodoList,
+    SingleDatePicker,
+    RightSide
+  },
+  methods: {
+    fetchData() {
+      this.error = this.post = null;
+      this.loading = true;
+      fetch("https://type.fit/api/quotes")
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(data) {
+          console.log(data);
+          this.quotes = data;
+        });
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+.calendar {
+  text-align: center;
+  margin-top: 250px;
+  padding-left: 30px;
+  justify-content: center;
+}
+</style>
